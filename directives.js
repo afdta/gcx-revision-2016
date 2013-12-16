606334a3b4f3f3f19472f69e1e9fa2b775a2edc2
HR <hrbuilder@hotmail.com>
2013-12-16 18:39:05 -0500

    Made location of metro area overviews more prominent. Tweaked map layout, removed extra padding, changed responsive breaks.

--- directives.js
+++ directives.js
@@ -1,12 +1,12 @@
-angular.module('directives',[]).directive('usMap', ['d3Service',function(d3){
+angular.module('directives',[]).directive('usMap', ['d3Service','$window',function(d3,$window){
   return {
-    template:'<div style="width:100%;min-width:300px;min-height:260px"></div>',
+    template:'<div style="width:100%;min-width:300px;min-height:260px;height:100%"></div>',
     replace:false,
     scope:false,
     link:function(s,i,a){
-      //console.log(s);
+      //console.log(i);
       if(d3){
-        var svg = d3.select(i.find('div')[0]).append("svg").style({"height":"260px","width":"120%"});
+        var svg = d3.select(i.find('div')[0]).append("svg").style({"height":"100%","width":"100%"});
 
         var defs = svg.append("svg:defs");
         var gradient2 = defs.append("svg:radialGradient").attr("id", "gradient2").attr("cx", "50%").attr("cy", "50%").attr("r", "50%").attr("fx", "45%").attr("fy", "45%");
@@ -45,19 +45,66 @@ angular.module('directives',[]).directive('usMap', ['d3Service',function(d3){
         var stjson = s.stjson;
         var latlon = s.latlon;
 
-        var proj = d3.geo.albersUsa().scale(500).translate([182,115]);
-        var path = d3.geo.path().projection(proj);
+        var proj = d3.geo.albersUsa();  
+        var path = d3.geo.path(); 
+
+        i.css("overflow","visible");
+        s.rescale = function(){
+          var w = i[0].offsetWidth <= 710 ? i[0].offsetWidth : 710;
+          //console.log(w);
+          var h = w*(.64);
+          var dots = [4,5.5,7];
+          var fs = ["9px","9px","11px"];
+          s.mapDim = {scale:w*1.32, translate:[(w/2)-8,(h/2)-8]};
+          var sml = s.mapDim.scale < 600 ? 0 : (s.mapDim.scale < 800 ? 1 : 2); //small, medium, or large: 1,2, or 3
+          s.mapDim.r = dots[sml];
+          s.mapDim.fs = fs[sml];
+          s.mapDim.level = sml; 
+          i.css("height",h+"px");
+          //i.css("width",w+"px");
+          //svg.style({"height":h,"width":w});
+          proj.scale(s.mapDim.scale).translate(s.mapDim.translate);
+          path.projection(proj);
+        }
+        s.rescale();
+
+        s.redraw = function(){
+          s.rescale();
+          borders.selectAll("path").attr("d",path(stjson));
+          dots.selectAll("circle")
+            .attr("cx",function(d,i){
+              var ll = latlon.metros[d];
+              ll.lonlat = proj([ll.Lon,ll.Lat]); //mutate original data
+              return ll.lonlat[0];
+            })
+            .attr("cy",function(d,i){
+              return latlon.metros[d].lonlat[1];
+            })
+            .attr("r",function(d,i){
+              if(d in s.gcxmetros){return s.mapDim.r;}
+              return 0;
+            });
+          dots.selectAll("path").attr("d",function(d,i){
+            var ll = latlon.metros[d];
+            var M = (Math.round(ll.lonlat[0])-1) + "," + Math.round(ll.lonlat[1])
+            return "M"+M+' l'+1+","+0;         
+          })
+          anno.selectAll("text")
+            .attr("x",function(d,i){return latlon.metros[d].lonlat[0];})
+            .attr("y",function(d,i){return latlon.metros[d].lonlat[1];})
+            .style("font-size",s.mapDim.fs);
+        }
+
         //var u = Math.sqrt(0.5);
-        borders.append("path").attr("d",path(stjson)).style({"fill":"#163A4C","stroke":"rgba(0,0,0,0)","stroke-width":"0.5px"});
+        borders.append("path").attr("d",path(stjson)).style({"fill":"#B8C0CC","stroke":"rgba(255,255,255,0)","stroke-width":"0.5px"});
         dots.selectAll("path").data(latlon.metroUniverse).enter().append("path").attr("d",function(d,i){
           var ll = latlon.metros[d];
           ll.lonlat = proj([ll.Lon,ll.Lat]); //mutate original data
           var M = (Math.round(ll.lonlat[0])-1) + "," + Math.round(ll.lonlat[1])
           return "M"+M+' l'+1+","+0;         
         })
-        .style({"fill":"none","stroke":"#B8B480","stroke-width":"1","shape-rendering":"crispEdges"});
+        .style({"fill":"none","stroke":"#ffffff","stroke-width":"1px","shape-rendering":"crispEdges"});
         
-        var radius = 3.5;
         dots.selectAll("circle").data(latlon.metroUniverse).enter().append("circle")
             .attr("cx",function(d,i){
               var ll = latlon.metros[d];
@@ -68,19 +115,30 @@ angular.module('directives',[]).directive('usMap', ['d3Service',function(d3){
               return latlon.metros[d].lonlat[1];
             })
             .attr("r",function(d,i){
-              if(d in s.gcxmetros){return radius;}
+              if(d in s.gcxmetros){return s.mapDim.r;}
               return 0;
             })
             .style("fill",function(d,i){
+              //return "url(#gradient2)";
               if(d in s.gcxmetros){return "#FFB33C"}
-              return "rgba(255,250,178,0.25)";
+              return "rgba(255,250,178,1)";
             })
             ;
         var gcxDots = dots.selectAll("circle").select(function(d,i){
           if(d in s.gcxmetros){return this}
           else{return null}
         })
+        gcxDots.style({"stroke":"#ffffff","stroke-width":"1px"})
         //.attr("filter","url(#smallShadow)")
+
+        anno.selectAll("text").data(latlon.metroUniverse).enter().append("text")
+            .attr("x",function(d,i){return latlon.metros[d].lonlat[0];})
+            .attr("y",function(d,i){return latlon.metros[d].lonlat[1];})
+            .text(function(d,i){
+              return (d in s.gcxmetros) ? latlon.metros[d].firstcity : "";
+            })
+            .attr({"dx":"-4px","dy":"4px"})
+            .style("font-size",s.mapDim.fs);
 
         //setTimeout(function(){gcxDots.attr("r",25);},1500);
         gcxDots.on("mouseover",function(d,i){
@@ -91,41 +149,46 @@ angular.module('directives',[]).directive('usMap', ['d3Service',function(d3){
           var r = parseInt(thiz.attr("r"));
           var attr = {r:r+8,cx:cx,cy:cy}
 
-          thiz.attr({"r":"8"})
+          thiz.transition().duration(250).attr({"r":r+5})
           
-          var c = anno.selectAll("circle.hoverDot").data([attr]);
-          var g = anno.selectAll("g.hoverDot").data([0]);
+          //var c = anno.selectAll("circle.hoverDot").data([attr]);
+          //var g = anno.selectAll("g.hoverDot").data([0]);
 
-          //c.enter().append("circle"); c.exit().remove(); c.attr("class","hoverDot").style({"fill":"none","stroke":"#ffffff","stroke-width":"1px","opacity":1}).attr(attr);
-          g.enter().append("g"); g.exit().remove(); g.attr("class","hoverDot").style("opacity",0).attr("transform","translate("+(cx+13)+","+(cy-8)+")");
-          
-          var rct = g.append("rect").attr({height:"16px",width:"100px","rx":"3px","ry":"3px"}).style({"fill":"#FFB33C","opacity":"1"});
-          var txt = g.append("text").attr({dx:"5px",dy:"12px"}).text(m.firstcity).style({"fill":"#303030","stroke":"none","stroke-width":"0.25px","font-size":"11px","font-weight":"bold","line-height":"11px"});
+          //g.enter().append("g"); g.exit().remove(); g.attr("class","hoverDot").style("opacity",0).attr("transform","translate("+(cx+13)+","+(cy-8)+")");
+          //var rct = g.append("rect").attr({height:"16px",width:"100px","rx":"3px","ry":"3px"}).style({"fill":"#FFB33C","opacity":"0"});
+          //var txt = g.append("text").attr({dx:"5px",dy:"12px"}).text(m.firstcity).style({"fill":"#303030","stroke":"none","stroke-width":"0.25px","font-size":"15px","font-weight":"normal","line-height":"15px"});
 
-          //marker is centered on a 22w x 40h canvas (pixels)
-          //var dMarker="M 10.557959,37.523391 C 9.8940192,31.186197 7.3594442,25.218177 4.0931601,19.882557 2.3532946,16.866978 0.67218032,13.377735 1.4097777,9.7570219 2.0655135,5.8675452 4.9354251,2.3728215 8.7116145,1.5343964 12.938731,0.38924059 17.817363,2.5719707 19.689533,6.7361 c 1.76022,3.598508 1.289455,8.1014 -0.851685,11.413865 -2.345186,4.317044 -4.986967,8.561833 -6.179866,13.42106 -0.666632,2.247227 -1.137764,4.561633 -1.337774,6.905799 -0.474939,0.90641 -0.87879,-0.553575 -0.762249,-0.953433 z"
-          //g.append("path").attr("d",dMarker).attr("transform","translate(-24,-35)").style({"pointer-events":"none","fill":"#CC3E14","stroke":"#ffffff","stroke-width":"0"});
+          //var w = txt.node().getBoundingClientRect();
+          //w.w = w.right-w.left;
+          //rct.attr("width",w.w+10);
 
-          var w = txt.node().getBoundingClientRect();
-          w.w = w.right-w.left;
-          rct.attr("width",w.w+10);
-
-          g.transition().delay(0).duration(100).style("opacity",1);
+          //g.transition().delay(0).duration(50).style("opacity",1);
         });
 
         gcxDots.on("mouseout",function(d,i){
-          d3.select(this).style({"stroke":"#ffffff","stroke-width":"0px"}).attr({"r":radius})
+          d3.select(this).style({"stroke":"#ffffff","stroke-width":"1px"}).transition().duration(50).attr({"r":s.mapDim.r})
           anno.selectAll(".hoverDot").remove();
         });
 
+        var searchBox = document.getElementById("metroSelectBox")
         gcxDots.on("mousedown",function(d,i){
           s.$apply(s.metro=d);
+          d3.select(searchBox).transition().duration(20).style("background-color","#FFB33C").each("end",function(){
+            d3.select(this).transition().delay(200).duration(500).style("background-color","#ffffff");
+          })
         });
+
+        //RESIZING
+        window.onresize = function() {
+          s.$apply(function(s){
+            s.redraw();
+          });
+        };
 
       }//end d3 code
       else{
         var jq = i.find('div')
-        jq.append('<img src="' + s.assetRepo + '/gcxmap.png"' + 'height="265" width="395"/>')
+        jq.append('<img src="' + s.assetRepo + '/gcxmap.png"/>')
         //("background-image","url(" + s.assetRepo + "/gcxmap.png" + ")");
         //jq.css("background-repeat","url(no-repeat)");
         //jq.css("background-repeat","left top");
