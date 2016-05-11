cd824ccf5e780e2777ff95cafd7aa15729b40d98
Alec <afriedhoff@live.com>
2016-05-11 12:01:42 -0400

    Made filter by plan type more robust.

--- archive/read_in_feed_data_from_metro_solutions_map.R
+++ archive/read_in_feed_data_from_metro_solutions_map.R
@@ -2,6 +2,22 @@ feed <- read.csv("~/Projects/Brookings/metro-innovations/feed/2016_04_14.csv", s
 
 feed$dt <- strptime(feed$date, format="%m/%d/%Y")
 
-feed2 <- feed[feed$type=="Plan", c("notes", "dt", "description", "link")]
+feed2 <- feed[gsub("\\s", "", feed$type)=="Plan", c("notes", "dt", "description", "link")]
 
 feed2 <- feed2[order(feed2$dt, decreasing=TRUE), ]
+
+
+#compare gcx data
+
+old <- read.csv("~/Projects/Brookings/gcx/GCXPlans_8Mar2015.csv", stringsAsFactors=FALSE)
+new <- read.csv("~/Projects/Brookings/gcx/GCXPlans_11May2016.csv", stringsAsFactors=FALSE)
+
+all.equal(old, new)
+
+emis <- old$Export_Plan != new$Export_Plan
+fmis <- old$Global_Trade_and_Investment_Plan != new$Global_Trade_and_Investment_Plan
+
+mismatches <- data.frame(oexp=old[emis, "Export_Plan"], 
+                         nexp=new[emis, "Export_Plan"])
+mismatches2 <- data.frame(ofdi=old[fmis, "Global_Trade_and_Investment_Plan"], 
+                          nfdi=new[fmis, "Global_Trade_and_Investment_Plan"])
